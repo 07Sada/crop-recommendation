02d88544349078bbaf70f0ff8fd29272d2fe382b
07Sada <sadashiv.nandanikar07@gmail.com>
2023-07-24 20:03:38 +0530

    logger file updated

--- src/logger.py
+++ src/logger.py
@@ -14,6 +14,7 @@ os.makedirs(LOG_FILE_DIR, exist_ok=True)
 # Log file path
 LOG_FILE_PATH = os.path.join(LOG_FILE_DIR, LOG_FILE_NAME)
 
+
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
